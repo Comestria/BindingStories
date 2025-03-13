28461ac49f90da957827793dff915ff616d9abb6
Hugo Blunier <hugo.blunier@outlook.com>
2025-03-13 15:40:29 +0100

    Modified formulas

--- Resources/Code/ModifiedAvoFormulae.c
+++ Resources/Code/ModifiedAvoFormulae.c
@@ -1,7 +1,7 @@
 #include "include/gbafe.h"
 
 void ComputeBattleUnitAvoidRate(struct BattleUnit* bu) {
-    bu->battleAvoidRate = (bu->battleSpeed * 2) + bu->terrainAvoid + (bu->unit.lck * 2);
+    bu->battleAvoidRate = (bu->battleSpeed * 2) + bu->terrainAvoid + bu->unit.lck;
 
     if (bu->battleAvoidRate < 0)
         bu->battleAvoidRate = 0;
